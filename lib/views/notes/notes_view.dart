f5601fe9c50c3a0c01a3f3aff378caaf6fa30ce6
mnaibei <muchajulius@gmail.com>
2024-01-04 20:50:22 +0300

    Remove comment

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -28,12 +28,6 @@ class _NotesViewState extends State<NotesView> {
     super.initState();
   }
 
-  // @override
-  // void dispose() {
-  //   _notesService.close();
-  //   super.dispose();
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
